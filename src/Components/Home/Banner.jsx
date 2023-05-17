6fa9fc7f3c073ad27bed7e6b2e7a3fc8e9a380a1
Rashik <smarashik00@gmail.com>
2023-05-17 21:25:02 +0600

    first commit

---
+++ src/Components/Home/Banner.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <>
+      <section className="relative mt-10     bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
+        <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
+
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-3xl text-center">
+            <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+              Understand User Flow.
+              <span className="sm:block"> Increase Conversion. </span>
+            </h1>
+
+            <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+              illo tenetur fuga ducimus numquam ea!
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Banner;
---
+++ src/Components/Router/Router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import Home from "../Home/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+export default router;
